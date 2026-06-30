7e16241d951fe3d6222fce5896826bcee8d1dad5
Rim Zabarov <rim@zabarov.com>
2026-06-30 17:51:55 +0300

    Replace layout placeholder tests

---
+++ scripts/run-unit-tests.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$tests = [
+    __DIR__ . '/../tests/Unit/LayoutContractTest.php',
+    __DIR__ . '/../tests/Unit/LayoutFailsClosedTest.php',
+    __DIR__ . '/../tests/Unit/InMemoryLayoutRuntimeTest.php',
+    __DIR__ . '/../tests/Unit/InMemoryPageBuilderRuntimeTest.php',
+];
+
+foreach ($tests as $test) {
+    require $test;
+}
+
+echo "Larena Layout unit tests passed.\n";
